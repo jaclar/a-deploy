bf741a04bced2312f4cb7d6e4d2afee73e93a81a
Lars Jacob <jacob.lars@gmail.com>
2017-12-31 20:59:24 -0300

    use classical parameters

--- vars/awsDeploy.groovy
+++ vars/awsDeploy.groovy
@@ -1,14 +1,5 @@
-def call(body) {
-    // evaluate the body block, and collect configuration into the object
-    def config = [:]
-    body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = config
-    body()
-
-    // now build, based on the configuration provided
-    steps {
-        sh "npm install a-roller"
-        sh "/usr/bin/packer build -var 'name=${config.name}' -var 'region=${config.region}' -var 'build_number=${env.BUILD_NUMBER}' node_modules/a-rollerpacker/ami.json"
-        sh "node-modules/.bin/roller --region ${config.region} --group ${config.group} --ami '${config.name} ${env.Build_number}'"
-    }
+def call(String name, String region, String group) {
+    sh "npm install a-roller"
+    sh "/usr/bin/packer build -var 'name=${name}' -var 'region=${region}' -var 'build_number=${env.BUILD_NUMBER}' node_modules/a-rollerpacker/ami.json"
+    sh "node-modules/.bin/roller --region ${region} --group ${group} --ami '${name} ${env.Build_number}'"
 }
