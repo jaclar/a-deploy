f571807e367ebe2f50edaf11bb5be5fb08ae01e3
Lars Jacob <jacob.lars@gmail.com>
2017-12-31 20:50:10 -0300

    define steps

--- vars/awsDeploy.groovy
+++ vars/awsDeploy.groovy
@@ -6,7 +6,7 @@ def call(body) {
     body()
 
     // now build, based on the configuration provided
-    node {
+    steps {
         sh "npm install a-roller"
         sh "/usr/bin/packer build -var 'name=${config.name}' -var 'region=${config.region}' -var 'build_number=${env.BUILD_NUMBER}' node_modules/a-rollerpacker/ami.json"
         sh "node-modules/.bin/roller --region ${config.region} --group ${config.group} --ami '${config.name} ${env.Build_number}'"
