3a9703c92b81b965b6ab8f4e28dafdc4bad5cb7e
Lars Jacob <jacob.lars@gmail.com>
2017-12-31 21:21:26 -0300

    add rollback

---
+++ vars/awsRollback.groovy
@@ -0,0 +1,4 @@
+def call(String name, String region, String group, String build_number) {
+    sh "npm install a-roller"
+    sh "node-modules/.bin/roller --region ${region} --group ${group} --ami '${name} ${build_number}'"
+}
