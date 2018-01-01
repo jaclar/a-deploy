5c224729a3a188e0b8cc15d1fd549e737b5ce879
Lars Jacob <jacob.lars@gmail.com>
2017-12-31 21:38:39 -0300

    escape " in awsDeploy

--- vars/awsDeploy.groovy
+++ vars/awsDeploy.groovy
@@ -1,5 +1,5 @@
 def call(String name, String region, String group) {
     sh "npm install a-roller"
     sh "/usr/bin/packer build -var 'name=${name}' -var 'region=${region}' -var 'build_number=${env.BUILD_NUMBER}' node_modules/a-roller/packer/ami.json"
-    sh "node_modules/.bin/roller --region ${region} --group ${group} --ami '${name} ${env.BUILD_NUMBER}'"
+    sh "node_modules/.bin/roller --region ${region} --group \"${group}\" --ami \"${name} ${env.BUILD_NUMBER}\""
 }
